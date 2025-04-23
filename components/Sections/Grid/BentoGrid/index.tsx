f3a3aa93b5b6c3f8b50c74909943ef12d9632d24
rz_yang <yrzcode@hotmail.com>
2025-04-24 05:17:32 +0900

    fix import error

--- components/Sections/Grid/BentoGrid/index.tsx
+++ components/Sections/Grid/BentoGrid/index.tsx
@@ -4,85 +4,86 @@ import { Skills } from "./SkillsCell";
 import { ContactCell } from "./ContectCell";
 
 export const BentoGrid = ({
-  className,
-  children,
+	className,
+	children,
 }: {
-  className?: string;
-  children?: React.ReactNode;
+	className?: string;
+	children?: React.ReactNode;
 }) => {
-  return (
-    <div
-      className={cn(
-        "grid md:auto-rows-[18rem] grid-cols-1 md:grid-cols-3 gap-4 max-w-7xl mx-auto",
-        className
-      )}
-    >
-      {children}
-    </div>
-  );
+	return (
+		<div
+			className={cn(
+				"grid md:auto-rows-[18rem] grid-cols-1 md:grid-cols-3 gap-4 max-w-7xl mx-auto",
+				className,
+			)}
+		>
+			{children}
+		</div>
+	);
 };
 
 export type BentoGridItemProps = {
-  id: number;
-  className?: string;
-  title?: string | React.ReactNode;
-  description?: string | React.ReactNode;
-  img?: string;
-  imgClassName?: string;
-  titleClassName?: string;
-  spareImg?: string;
+	id: number;
+	itemId?: string;
+	className?: string;
+	title?: string | React.ReactNode;
+	description?: string | React.ReactNode;
+	img?: string;
+	imgClassName?: string;
+	titleClassName?: string;
+	spareImg?: string;
 };
 
 export const BentoGridItem = (props: BentoGridItemProps) => {
-  const {
-    id,
-    className,
-    title,
-    titleClassName,
-    description,
-    img,
-    spareImg,
-    imgClassName,
-  } = props;
+	const {
+		id,
+		className,
+		title,
+		titleClassName,
+		description,
+		img,
+		spareImg,
+		imgClassName,
+	} = props;
 
-  return (
-    <div
-      className={cn(
-        "group/bento",
-        "bento-grid-item",
-        id === 6 && "flex-center",
-        className
-      )}
-    >
-      {/* IMG */}
-      <div className="bento-grid-item__img">
-        {img && <img src={img} alt={img} className={imgClassName} />}
-      </div>
+	return (
+		<div
+			className={cn(
+				"group/bento",
+				"bento-grid-item",
+				id === 6 && "flex-center",
+				className,
+			)}
+		>
+			{/* IMG */}
+			<div className="bento-grid-item__img">
+				{img && <img src={img} alt={img} className={imgClassName} />}
+			</div>
 
-      {/* SPARE_IMG */}
-      <div className="bento-grid-item__imgSpare">
-        {img && <img src={spareImg} alt={spareImg} />}
-      </div>
+			{/* SPARE_IMG */}
+			<div className="bento-grid-item__imgSpare">
+				{img && <img src={spareImg} alt={spareImg} />}
+			</div>
 
-      {/* CONTENTS */}
-      <div className={cn(titleClassName, "bento-grid-item-contents")}>
-        {/* DESCRIPTION */}
-        <div className="bento-grid-item-contents__description">
-          {description}
-        </div>
+			{/* CONTENTS */}
+			<div className={cn(titleClassName, "bento-grid-item-contents")}>
+				{/* DESCRIPTION */}
+				<div className="bento-grid-item-contents__description">
+					{description}
+				</div>
 
-        {/* TITLE */}
-        <div className="bento-grid-item-contents__title">{title}</div>
+				{/* TITLE */}
+				<div className="bento-grid-item-contents__title">{title}</div>
 
-        {/* GLOBE EFFECT */}
-        {id === 2 && <GridGlobe />}
+				{/* GLOBE EFFECT */}
+				{id === 2 && <GridGlobe />}
 
-        {/* WORDS */}
-        {id === 3 && <Skills />}
+				{/* WORDS */}
+				{id === 3 && <Skills />}
 
-        {/* ANIMATION */}
-        {id === 6 && <ContactCell />}
-      </div>
-    </div>
-  );
+				{/* ANIMATION */}
+				{id === 6 && <ContactCell />}
+			</div>
+		</div>
+	);
 };
