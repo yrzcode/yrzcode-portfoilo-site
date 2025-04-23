00d69840287901fa5df04a2802c37388783aa46a
rz_yang <yrzcode@hotmail.com>
2025-04-24 08:00:20 +0900

    find build error test

--- app/page.tsx
+++ app/page.tsx
@@ -18,10 +18,10 @@ export default function HomePage() {
 				<Hero />
 				<Grid />
 				<Projects />
-				<Testimonials />
+				{/* <Testimonials />
 				<Experience />
 				<Approach />
-				<Footer />
+				<Footer /> */}
 			</div>
 		</main>
 	);
