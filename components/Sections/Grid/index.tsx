d8c979f16eba5722097aa039151f54750c171f74
rz_yang <yrzcode@hotmail.com>
2025-04-24 05:06:07 +0900

    fix import error

--- components/Sections/Grid/index.tsx
+++ components/Sections/Grid/index.tsx
@@ -1,16 +1,16 @@
 import React from "react";
-import { BentoGrid, BentoGridItem, BentoGridItemProps } from "../../ui";
+import { BentoGrid, BentoGridItem, BentoGridItemProps } from "./BentoGrid";
 import { gridItems } from "@/data";
 
 export const Grid = () => {
-  return (
-    <section id="#about">
-      <BentoGrid>
-        {gridItems.map((item, i) => {
-          const { id } = item;
-          return <BentoGridItem key={id} {...item} />;
-        })}
-      </BentoGrid>
-    </section>
-  );
+	return (
+		<section id="#about">
+			<BentoGrid>
+				{gridItems.map((item, i) => {
+					const { id } = item;
+					return <BentoGridItem key={id} {...item} />;
+				})}
+			</BentoGrid>
+		</section>
+	);
 };
