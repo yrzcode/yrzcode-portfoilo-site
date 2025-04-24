59c96a80dbda4e473d059e9fb73e3afb601af1be
rz_yang <yrzcode@hotmail.com>
2025-04-24 16:28:54 +0900

    fix build error

--- components/Sections/Grid/Grid.tsx
+++ components/Sections/Grid/Grid.tsx
@@ -1,15 +1,17 @@
+"use client";
+
 import { gridItems } from "@/data";
 import { BentoGrid, BentoGridItem } from "./BentoGrid/BentoGrid";
 
 export const Grid = () => {
-  return (
-    <section id="about">
-      <BentoGrid>
-        {gridItems.map((item, i) => {
-          const { id } = item;
-          return <BentoGridItem key={id} {...item} />;
-        })}
-      </BentoGrid>
-    </section>
-  );
+	return (
+		<section id="about">
+			<BentoGrid>
+				{gridItems.map((item, i) => {
+					const { id } = item;
+					return <BentoGridItem key={id} {...item} />;
+				})}
+			</BentoGrid>
+		</section>
+	);
 };
