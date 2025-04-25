be5818da7a1848ea4aefe958ab0ba684fdf4b1d8
rz_yang <yrzcode@hotmail.com>
2025-04-26 06:20:40 +0900

    change approach title

--- components/Sections/Approach/Title.tsx
+++ components/Sections/Approach/Title.tsx
@@ -1,10 +1,10 @@
 import { SectionHeading } from "@/components/ui";
 
 export const Title = () => {
-  return (
-    <SectionHeading className="mt-10 py-8">
-      My
-      <span className="text-purple"> approach</span>
-    </SectionHeading>
-  );
+	return (
+		<SectionHeading className="mt-10 py-8">
+			My
+			<span className="text-purple"> Approach</span>
+		</SectionHeading>
+	);
 };
