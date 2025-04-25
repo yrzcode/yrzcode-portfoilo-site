ea06ec52c9d2994a9aca2ae29e578fed97bc5b96
rz_yang <yrzcode@hotmail.com>
2025-04-26 08:09:16 +0900

    fix copyright text

--- components/Sections/Footer/Copyright.tsx
+++ components/Sections/Footer/Copyright.tsx
@@ -1,24 +1,26 @@
 import { socialMedia } from "@/data";
 
 export const Copyright = () => {
-  return (
-    <div className="copyright-container">
-      <p className="copyright">Copyright © 2024. Yang.</p>
-      <div className="social-media">
-        {socialMedia.map(({ id, img }) => {
-          return (
-            <div key={id}>
-              <img
-                src={img}
-                alt={img}
-                className="image"
-                height={20}
-                width={20}
-              />
-            </div>
-          );
-        })}
-      </div>
-    </div>
-  );
+	return (
+		<div className="copyright-container">
+			<p className="copyright">
+				Copyright © {new Date().getFullYear()} Renzheng Yang.
+			</p>
+			<div className="social-media">
+				{socialMedia.map(({ id, img }) => {
+					return (
+						<div key={id}>
+							<img
+								src={img}
+								alt={img}
+								className="image"
+								height={20}
+								width={20}
+							/>
+						</div>
+					);
+				})}
+			</div>
+		</div>
+	);
 };
