d96a0a04719def05c4557490fdf4204c0cdd45f4
rz_yang <yrzcode@hotmail.com>
2025-04-26 06:30:05 +0900

    adjust mobile layout

--- components/Sections/Experience/Title.tsx
+++ components/Sections/Experience/Title.tsx
@@ -3,7 +3,7 @@ import { SectionHeading } from "@/components/ui";
 export const Title = () => {
 	return (
 		<SectionHeading className="mt-10 py-8">
-			<span className="text-purple">My Work&nbsp;</span>
+			<span className="text-purple">Work&nbsp;</span>
 			<span className="text-yellow-400">Experience</span>
 		</SectionHeading>
 	);
--- components/Sections/Projects/Title.tsx
+++ components/Sections/Projects/Title.tsx
@@ -5,6 +5,7 @@ export function Title() {
 		<SectionHeading>
 			{/* A small selection of <br /> */}
 			<span className="text-purple">Recent Project&nbsp;&&nbsp;</span>
+			<br />
 			<span className="text-yellow-200">Highlights</span>
 		</SectionHeading>
 	);
