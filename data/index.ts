c0d3c3d252443622821e51fe788e9a3a1757d8fc
rz_yang <yrzcode@hotmail.com>
2025-04-26 04:15:41 +0900

    add recent project (my blog)

--- data/index.ts
+++ data/index.ts
@@ -80,11 +80,11 @@ export const gridItems: BentoGridItemProps[] = [
 export const projects = [
 	{
 		id: 1,
-		title: "3D Solar System Planets to Explore",
+		title: "Rz-Engineering Blog",
 		des: "Explore the wonders of our solar system with this captivating 3D simulation of the planets using Three.js.",
-		img: "/p1.svg",
+		img: "/images/blog.jpg",
 		iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
-		link: "#",
+		link: "https://www.yrzcode.com",
 	},
 	{
 		id: 2,
