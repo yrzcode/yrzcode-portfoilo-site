109c29ccba1706e770b89cd063dedd5edc7e3df3
rz_yang <yrzcode@hotmail.com>
2025-04-26 04:05:53 +0900

    change title to uppercase

--- components/Sections/Projects/Title.tsx
+++ components/Sections/Projects/Title.tsx
@@ -4,7 +4,7 @@ export function Title() {
 	return (
 		<SectionHeading>
 			{/* A small selection of <br /> */}
-			<span className="text-purple">recent project highlights</span>
+			<span className="text-purple">Recent Project Highlights</span>
 		</SectionHeading>
 	);
 }
